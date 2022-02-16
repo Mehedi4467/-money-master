f0ed8eec7aebbfe8305d2491f4d56e09347ecd9b
Mehedi4467 <mehedihassan4467@gmail.comm>
2022-02-17 02:21:25 +0600

    income and cost section error massage complated

--- js/main.js
+++ js/main.js
@@ -3,19 +3,51 @@ function getInputValue(inputId){
     const inputText = document.getElementById(inputId + '-input');
     const inputValue = inputText.value;
     const inputNumber = parseFloat(inputValue);
-    return inputNumber;
+    let massage = errorMassage(inputValue,inputId, inputNumber, inputText);
+    if(massage == true){
+        return inputNumber;
+    }
+    else{
+        return 0;
+    }
+    
 }
 
 // calculate function income and cost 
-function calculateIncomeAndCost(income,food,rend,clothes){
+function calculateIncomeAndCost(income, food, rend, clothes){
     const totalCostShow = document.getElementById('total-cost');
     const balanceShow = document.getElementById('balance');
     const totalCost = food + rend + clothes;
-    totalCostShow.innerText = totalCost;
     const balance = income - totalCost;
+    if(income < totalCost){
+        const incomeError = document.getElementById('incomeError');
+        incomeError.innerText = 'Please increase your income';
+    }
+    else{
+    totalCostShow.innerText = totalCost;
     balanceShow.innerText = balance;
+    incomeError.style.display = 'none';
+    }
+    
 }
 
+
+
+//error massage function 
+function errorMassage(inputValue, inputId, inputNumber, inputText){
+    const targetEorrorTag = document.getElementById(inputId + '-input-error');
+    if(isNaN(inputValue) || inputText.value == ''){
+        targetEorrorTag.innerText = 'Please input number !!'; 
+    }
+    else if(inputNumber < 0 ){
+        targetEorrorTag.innerText = 'Please input a number  greater then zero !!';
+    }
+    else{
+        targetEorrorTag.style.display = 'none';
+        return true;
+    }
+   
+}
 // income and cost Calculate
 
 document.getElementById('income_cost-button').addEventListener('click', function(){
@@ -23,5 +55,9 @@ document.getElementById('income_cost-button').addEventListener('click', function
     const foodNumber = getInputValue ('food');
     const rentNumber = getInputValue ('rent');
     const clothesNumber = getInputValue ('clothes');
-    calculateIncomeAndCost(incomeNumber, foodNumber, rentNumber, clothesNumber);
-})
+    if(incomeNumber !=0 || foodNumber != 0 || rentNumber !=0 || clothesNumber != 0){
+        calculateIncomeAndCost(incomeNumber, foodNumber, rentNumber, clothesNumber);
+    }
+    
+});
+
